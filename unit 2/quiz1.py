5266b899e7285026d900f697f91705e0f3452b49
jharvey079 <jharvey079@boyslatin.net>
2025-09-19 14:13:36 +0000

    finished quiz

---
+++ unit 2/quiz1.py
@@ -0,0 +1,63 @@
+# Create a new document called quiz1.py copy/ paste the following questions
+# on your document and answer the following questions.
+
+# quiz rules:
+# - There is no talking during the quiz
+# - You may only use your classnotes and w3schools.com for reference
+# - If you have a question about a quiz question, please raise your hand
+# - Once finished, submit your code to your repository using the source control 
+# button. Your commit should be "completed quiz 1."
+
+# 1. Name and describe three (3) built-in functions in python. 
+"print, input function, string"
+# Please write your responses in complete sentences.
+
+# 2. What data type will the following snippet of code output?
+print(30/ 232)
+"0.12931034482758622"
+
+# 3.  Identify and name the operator family each of the following
+# symbols belongs to.
+
+# and
+"	Returns True if both statements are true	x < 5 and  x < 10"
+# ==
+"	Equal	x == y"
+# >
+"Greater than	x > y"
+
+# 4. Explain the difference between the = operator and the == operator.
+# Please write your response in complete sentences.
+"the = operator 	x = 5	x = 5 while the == operator is 	Equal	x == y"
+
+# 5. Write code that takes a user’s input (as a string), 
+# casts it to a float, and prints the result multiplied by 2.
+"x = float(2)"
+
+# 6. What is the difference between a parameter and an argument?
+# Please write your response in complete sentences.
+"A parameter is a value,	a number or a string that can be converted into a floating point number and an argument is what removes that converting floating point number"
+
+# 7. What is the difference between a function definition 
+# and a function invocation? # Please write your response in 
+# complete sentences.
+"In Python a function is defined using the def keyword: and a function invocation is what calls the function"
+
+# 8. Why are functions useful in programming? Provide at least two reasons 
+# and write your reasons in in complete sentences.
+"Function are useful in programming because they organize things and they can make things work, they can calculate stuff and have inputs and outputs."
+
+# 9. Write a code block that uses the appropriate operator for each scenario
+x = 15
+y = 20.
+# x is greater than y
+# x and 15 are both the same
+# x and y are not the same`
+"print(x=20, y=15"
+
+# 10. Create a function that will take in two values. Your function should
+# take in 1 value as a parameter, and the other value should be passed in by the
+# user through the terminal. Your function should compare if the number passed in by
+# the user is greater than the number passed in via the parameter and should print out
+# the appropriate response of true or false.
+"def my_function(): print("true or false")"
