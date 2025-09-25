e8d51fb8a3576a3c58533b40aeb26262c6910bc1
jharvey079 <jharvey079@boyslatin.net>
2025-09-25 14:26:52 +0000

    conditions work

---
+++ unit 2/conditions.py
@@ -0,0 +1,46 @@
+# Tell me what dto do on each down based on the yards.
+down = input('What down is it? ')
+yards = input('How many yards do you need to get another first down? ')
+
+if down ==1 and yards <= 5:
+    print("run it.")
+elif down ==2 and yards <= 5:
+    print("run it")
+elif down ==3 and yards <= 5:
+    print("play action")
+elif # this is the exit; assu,es it is 4th down.
+    print("punt")
+
+
+def testTakePermit(Age):
+  if age > 16:
+    print('You may take your permit test.')
+  else:
+     print('You are not old enough')
+
+testTakePermit(17)
+
+
+
+def gradeScoreChecker(number):
+  if below > 70:
+    print('You have an F.')
+
+    gradeScoreChecker(70)
+def score(positive):
+  elif number > 75:
+    print('You have a C')
+
+     gradeScoreChecker(75)
+    def score(positive):
+  elif number > 80:
+     
+     gradeScoreChecker(80)
+    print('You have an B.')
+    def score(positive):
+  else number > 90:
+    print('You have an A.')
+    
+     gradeScoreChecker(90)
+
+
