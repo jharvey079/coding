9566022450fa79575ec3d853d0e0551f3c26b1b1
jharvey079 <jharvey079@boyslatin.net>
2025-11-06 15:27:08 +0000

    11/6/25

---
+++ unit 3/forLoops.py
@@ -0,0 +1,106 @@
+# For Loops - A type of construct that runs code instructions
+# a finite amount of times over a group of data.
+
+
+
+
+
+
+
+
+
+#halloweenBag =['Snickers','Hershey Bar','Twizzler','Candied Apple','Candy Corn']
+
+#count = len(halloweenBag)
+#print(count)
+
+
+
+#for candy in halloweenBag:
+    #print(candy)
+    #print('I got '+candy+' in my bag ')
+
+
+
+
+
+ 
+    
+#for x in range(5):
+    #word = input("Please type in a word: ")
+    #print(word)
+
+
+
+
+
+
+# looping through strings
+
+#word = "Python"
+#for letter in word:
+    #print(letter)
+
+
+
+
+
+
+
+# looping through lists of numbers
+
+#shoppingPrices = [3.00, 5.40, 7.20, 9.00, 10.40, 11.00]
+#total = 0
+
+#for items in shoppingPrices:
+    #total += items
+    #print(total)
+
+#print(total)
+
+
+
+
+
+
+
+
+
+
+studentBody = ['jordan','joel','devonte'],',,,'
+
+present =[]
+absent=[]
+scannedIn = True
+for student in studentBody:
+    print('is '+ student + 'scanned in today?')
+    response = input()
+
+#if student =1 scannedIn
+#move to absent list
+#absent .append(student)
+#else put them in the present list
+#present. append(student)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
