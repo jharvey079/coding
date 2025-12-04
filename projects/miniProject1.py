adf48217fdb5aeffe2df59740c06389da08e7a3a
jharvey079 <jharvey079@boyslatin.net>
2025-12-04 15:26:24 +0000

    mini project (with 10 questions)

--- projects/miniProject1.py
+++ projects/miniProject1.py
@@ -37,8 +37,43 @@ def quiz():
         scoreTracker = 5
     else:
         print("incorrect the answer was 1500")
+    print("Question 6: what is 25+75?")
+    questionSix = input()
+    if int(questionSix) == 100:
+            print("correct + 1 point")
+            scoreTracker = 6
+    else:
+            print("incorrect the answer was 100")
+    print("Question 7: what is 9+10?")
+    questionSeven = input()
+    if int(questionSeven) == 19:
+            print("incorrect the answer was 21. (im just playing) + 1 point")
+            scoreTracker = 7
+    else:
+            print("incorrect the answer was 19")
+    print("Question 8: what is 3000+6000?")
+    questionEight = input()
+    if int(questionEight) == 9000:
+            print("correct + 1 point")
+            scoreTracker = 8
+    else:
+            print("incorrect the answer was 9000")
+    print("Question 9: what is 10000 + 10000?")
+    questionNine = input()
+    if int(questionNine) == 20000:
+            print("correct + 1 point")
+            scoreTracker = 9
+    else:
+            print("incorrect the answer was 20000")
+    print("Question 10: what is 5x5?")
+    questionTen = input()
+    if int(questionTen) == 25:
+            print("correct + 1 point")
+            scoreTracker = 10
+    else:
+        print("incorrect the answer was 25")
     print("the quiz is finished thank you for playing")
-    print("your final score is " + str(scoreTracker) + " out of 5")
+    print("your final score is " + str(scoreTracker) + " out of 10")
 
 quiz()
 
